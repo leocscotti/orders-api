1587da90577fa8c25631c7627ab23b4087e68a4a
leocscotti <leoscotti03@gmail.com>
2026-03-09 22:48:58 -0300

    feat: documentação com swagger configurada

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 
+const {swaggerUI, swaggerSpec}= require("./docs/swagger");
+
 app.use(express.json());
 
 app.use("/order", orderRoutes);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 module.exports = app;
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,14 +3,110 @@ const router = express.Router();
 
 const controller = require("../controllers/orderController");
 
+/**
+ * @swagger
+ * /order:
+ *   post:
+ *     summary: Criar um novo pedido
+ *     tags: [Orders]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               numeroPedido:
+ *                 type: string
+ *               valorTotal:
+ *                 type: number
+ *               dataCriacao:
+ *                 type: string
+ *               items:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     idItem:
+ *                       type: string
+ *                     quantidadeItem:
+ *                       type: number
+ *                     valorItem:
+ *                       type: number
+ *     responses:
+ *       201:
+ *         description: Pedido criado com sucesso
+ */
 router.post("/", controller.createOrder);
 
+/**
+ * @swagger
+ * /order/list:
+ *   get:
+ *     summary: Listar todos os pedidos
+ *     tags: [Orders]
+ *     responses:
+ *       200:
+ *         description: Lista de pedidos
+ */
 router.get("/", controller.listOrders);
 
+/**
+ * @swagger
+ * /order/{id}:
+ *   get:
+ *     summary: Buscar pedido por ID
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pedido encontrado
+ *       404:
+ *         description: Pedido não encontrado
+ */
 router.get("/:id", controller.getOrder);
 
+/**
+ * @swagger
+ * /order/{id}:
+ *   put:
+ *     summary: Atualizar um pedido
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pedido atualizado
+ *       404:
+ *         description: Pedido não encontrado
+ */
 router.put("/:id", controller.updateOrder);
 
+/**
+ * @swagger
+ * /order/{id}:
+ *   delete:
+ *     summary: Deletar um pedido
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pedido removido
+ */
 router.delete("/:id", controller.deleteOrder);
 
 module.exports = router;
